ab62b60148e78d2ed2f6cfc2326e1e4f16411a7b
Rachel Rattay <ror5226@psu.edu>
2017-02-25 17:04:50 -0500

    Added UI

---
+++ HarassmentSimulation/Assets/ButtonFunctionality.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunctionality : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NextButton_Pressed()
+    {
+
+        GameObject description_text = GameObject.FindGameObjectWithTag("Description_Text");
+        if(description_text != null)
+        {
+            Text desc_text = description_text.GetComponent<Text>();
+            desc_text.text = " hello";
+        }
+        else
+        {
+            Debug.Log("Couldnt find it");
+        }
+    }
+}
