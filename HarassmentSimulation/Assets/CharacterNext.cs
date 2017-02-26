152959ed8ba9744b34023b465f9dd2feae71296c
Rachel Rattay <ror5226@psu.edu>
2017-02-26 00:16:05 -0500

    Current Status

---
+++ HarassmentSimulation/Assets/CharacterNext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterNext : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Button_Pressed()
+    {
+        CharacterSelect.game.transform.position = CharacterSelect.charact.transform.position;
+        CharacterSelect.charact.SetActive(false);
+    }
+}
--- HarassmentSimulation/Assets/CharacterSelect.cs
+++ HarassmentSimulation/Assets/CharacterSelect.cs
@@ -7,23 +7,31 @@ public class CharacterSelect : MonoBehaviour {
     public char character = 'k';
     public static GameObject menu;
     public static GameObject game;
+    public static GameObject charact;
 
     // Use this for initialization
     void Start () {
         game = GameObject.FindGameObjectWithTag("Main_Game");
         menu = GameObject.FindGameObjectWithTag("Main_Menu");
+        charact = GameObject.FindGameObjectWithTag("Main_Character");
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void Character_Selected()
     {
-        PathManager.pathName += character;
+        PathManager pathManager = new PathManager();
 
-        game.transform.position = menu.transform.position;
+        PathManager.pathName += character;
+        PathManager.pathName += "1";
+
+        pathManager.Update_Text();
+
+        charact.transform.position = menu.transform.position;
         menu.SetActive(false);
     }
 }
